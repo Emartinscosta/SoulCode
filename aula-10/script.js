eb5f2e316a1cd70ab560f23de6475a58b406df8a
Emartinscosta <emartins.costa@hotmail.com>
2024-11-04 19:38:22 -0300

    atualização

---
+++ aula-10/script.js
@@ -0,0 +1,100 @@
+console.log("Olá mundo II")
+
+// comentario de linha
+
+/*comentario de varias linhas*/
+
+let nome = "Clarice";;
+let idade = 17;
+let nota = 8.5;
+
+idade = 25;
+nota = 7;
+
+console.log(nome, idade, nota)
+
+// const = maskCPF = "000.000.000-00"
+
+
+let tipo = "lorem";
+tipo = 24;
+tipo = false;
+console.log(typeof tipo)
+tipo = null;
+console.log(typeof tipo)
+let =  tipo;
+console.log(tipo);
+
+// Operadores
+/*let a = 5, b = 3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+*/
+/*const x = 12, y = 6;
+console.log(x + y);
+console.log(y - x);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+*/
+// console.log(1000*25/100);
+console.log(5 / 3);
+const per = 75;
+const num = 200;
+console.log(num *(per / 100));
+
+let a = 5, b = 6, c = 3;
+let delta = (b ** 2) + (4 * a * c);
+console.log(delta);
+
+// operador de atribuição
+
+let numero =7; //12
+numero += 5; //36
+numero *= 3; //4
+numero -= 12; //-8
+numero /= 9; //4
+
+console.log(numero);
+
+numero = 2;
+
+console.log(numero++); //primero executa o comando depois soma ou atribui o valor
+console.log(++numero);
+console.log(numero--);
+console.log(--numero);
+numero++;
+console.log(numero);
+
+// operadores de Comparação
+
+console.log (6 == 6);
+console.log ("6" == 6);
+console.log ("6" === 6);
+console.log (4 > 7);
+console.log (4 < 7);
+console.log (9 >= 9);
+console.log (7 < 7);
+console.log (7 <= 7);
+console.log ("6" !== 6);
+
+console.log ("Gabriel" == "gabriel");
+console.log ("Gabriel" != "gabriel");
+
+// Operadores Logicos
+//Conjução - E - AND - %%
+//Disjunção - OU - OUR - ||
+//Negação - Não - NOT - !
+let p = true;
+let q =false
+console.log (p && q);
+console.log (p || q);
+console.log (p && p);
+console.log (!p || q);
+console.log (!(p || q));
+
