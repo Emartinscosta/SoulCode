8fbcfc3dbcee0247277e67dc48f952ab788b1784
Emartinscosta <emartins.costa@hotmail.com>
2024-11-05 09:05:55 -0300

    atividadeaula-10

---
+++ atividades/atividadeAula-10/index.js
@@ -0,0 +1,63 @@
+//1.
+/*let x = 7, y = 8;
+console.log(x + y);  //15
+console.log(x - y);  //-1
+console.log(x * y);  //56
+console.log(x / y);  //0.875
+*/
+
+/*
+let x = 10;
+
+x += 5; 
+console.log(x); //15
+
+x -= 3; 
+console.log(x); //12
+
+x *= 2; 
+console.log(x); //24
+
+x /= 4; 
+console.log(x); //6
+*/
+
+//3.
+/*console.log (72 == "72"); //true
+console.log (72 === "72"); //false
+console.log("SoulCode" != "souldode"); //true
+*/
+
+//4.
+/*console.log(545 >= 748); //false
+console.log(25 <= 96); //true
+*/
+
+//5.
+// Valores booleanos
+let a = true;
+let b = false;
+let c = true;
+
+// Conjunção de todos eles (a && b && c)
+let conjuncao = a && b && c;
+
+// Disjunção de todos eles (a || b || c)
+let disjuncao = a || b || c;
+
+// Negação da disjunção de todos eles (! (a || b || c))
+let negacaoDisjuncao = !(a || b || c);
+
+// Exibindo os resultados
+console.log("Conjunção de todos eles (a && b && c):", conjuncao);
+console.log("Disjunção de todos eles (a || b || c):", disjuncao);
+console.log("Negação da disjunção de todos eles (! (a || b || c)):", negacaoDisjuncao);
+
+
+
+
+
+
+
+
+
